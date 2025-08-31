b45f86301966e130efd0a212c2170860d0691dcf
Web4 <167559384+Web4application@users.noreply.github.com>
2025-08-31 15:58:56 -0700

    Create types.ts

---
+++ app/api/openai/types.ts
@@ -0,0 +1,32 @@
+import { MultimodalContent } from "../api";
+import { ModelSize, DalleQuality, DalleStyle } from "@/app/typing";
+
+export interface OpenAIListModelResponse {
+  object: string;
+  data: Array<{ id: string; object: string; root: string }>;
+}
+
+export interface RequestPayload {
+  messages: {
+    role: "developer" | "system" | "user" | "assistant";
+    content: string | MultimodalContent[];
+  }[];
+  stream?: boolean;
+  model: string;
+  temperature: number;
+  presence_penalty: number;
+  frequency_penalty: number;
+  top_p: number;
+  max_tokens?: number;
+  max_completion_tokens?: number;
+}
+
+export interface DalleRequestPayload {
+  model: string;
+  prompt: string;
+  response_format: "url" | "b64_json";
+  n: number;
+  size: ModelSize;
+  quality: DalleQuality;
+  style: DalleStyle;
+}
