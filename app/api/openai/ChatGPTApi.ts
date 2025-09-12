11261165a1af00a0adcb12965d24bc721dfec00a
Web4 <167559384+Web4application@users.noreply.github.com>
2025-09-12 10:22:41 -0700

    Update ChatGPTApi.ts

--- app/api/openai/ChatGPTApi.ts
+++ app/api/openai/ChatGPTApi.ts
@@ -5,13 +5,45 @@ import { speech } from "./speech";
 import { usage } from "./usage";
 import { models } from "./models";
 
-import { LLMApi, ChatOptions, SpeechOptions, LLMUsage, LLMModel } from "../api";
+import { image } from "./image";
+import { translate } from "./translate";
+import { speechToText } from "./speechToText";
+import { textToSpeech } from "./textToSpeech";
+import { recognition } from "./recognition";
+
+import {
+  LLMApi,
+  ChatOptions,
+  SpeechOptions,
+  LLMUsage,
+  LLMModel,
+  ImageOptions,
+  TranslateOptions,
+  SpeechToTextOptions,
+  TextToSpeechOptions,
+  RecognitionOptions
+} from "../api";
 
 export class ChatGPTApi implements LLMApi {
   chat = chat;
   speech = speech;
   usage = usage;
   models = models;
+  image = image;
+  translate = translate;
+  speechToText = speechToText;
+  textToSpeech = textToSpeech;
+  recognition = recognition;
 }
 
-export { chat, speech, usage, models };
+export {
+  chat,
+  speech,
+  usage,
+  models,
+  image,
+  translate,
+  speechToText,
+  textToSpeech,
+  recognition
+};
