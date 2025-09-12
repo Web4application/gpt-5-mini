1bb8634570949909f50a8a47c4e65b885f5a6b85
Web4 <167559384+Web4application@users.noreply.github.com>
2025-09-12 08:40:32 -0700

    Create bash.sh

---
+++ Frontend/bash.sh
@@ -0,0 +1,12 @@
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+cd frontend
+npm start
+cd backend
+node server.js
+npx create-react-app frontend
+cd frontend
+npm install
+npm init -y
+npm install express cors dotenv openai
+node server.js
